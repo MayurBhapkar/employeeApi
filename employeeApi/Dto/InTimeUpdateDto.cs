193ffe75935c3b07ae56d9d75b6c92f23a88bac4
Mayur Bhapkar <mayurbhapkar2001@gmail.com>
2025-06-24 16:29:49 +0530

    updateinout

---
+++ employeeApi/Dto/InTimeUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace employeeApi.Dto
+{
+    public class InTimeUpdateDto
+    {
+        public string name { get; set; }
+        public TimeOnly inTime { get; set; }
+        public TimeOnly outTime { get; set; }
+        public string totalHrs { get; set; }
+    }
+}
